5d3497e3b759430758c81a310d40df52cb20f404
Anas Asimi <anas2020cr7@gmail.com>
2023-05-23 22:26:41 +0100

    complete cube interactivity

--- main.js
+++ main.js
@@ -1,54 +1,79 @@
 class Cube {
-    isMouseEnter = false
-    isMouseDown = false
-    isMouseMove = false
-    previousPosition = {
-        x: null,
-        y: null,
-    }
-    constructor(cube) {
-        this.cube = cube
-        this.cube.style.transform = 'rotateX(0deg) rotateY(0deg)'
-        // isMouseEnter
-        this.cube.addEventListener('mouseenter', () => {
-            if (this.isMouseDown) return
-            console.log('mouseenter');
-            this.isMouseEnter = true
-        })
-        this.cube.addEventListener('mouseleave', () => {
-            if (this.isMouseDown) return
-            console.log('mouseleave');
-            this.isMouseEnter = false
-        })
-        // isMouseDown
-        window.addEventListener('mousedown', (e) => {
-            console.log('mousedown');
-            this.isMouseDown = true
-            let [x, y] = this.cube.style.transform.split(' ').map(i => Number(i.slice(i.indexOf('(') + 1, i.indexOf('deg'))))
-            this.previousPosition = { x, y }
-        })
-        window.addEventListener('mouseup', () => {
-            console.log('mouseup');
-            this.isMouseDown = false
-            this.isMouseEnter = false
-        })
-        // 
-        window.addEventListener('mousemove', (e) => {
-            if (this.isMouseEnter && this.isMouseDown) {
-                let deltaX = this.previousPosition.x - e.clientX
-                let deltaY = this.previousPosition.y - e.clientY
-                this.#updateRotation(deltaX, deltaY)
-            }
-        })
-    }
-    #updateRotation(deltaX, deltaY) {
-        // get previous x,y
-        let { x, y } = this.previousPosition
-        // set new ones
-        this.cube.style.transform = `rotateX(${y + deltaY}deg) rotateY(${x - deltaX}deg)`
-    }
+	// intialize
+	isMouseEnter = false;
+	isMouseDown = false;
+	isMouseMove = false;
+	transforming_SCALE = 2
+	previousTransform = {
+		x: 0,
+		y: 0,
+	};
+	positionInStart = {
+		x: 0,
+		y: 0,
+	};
+	// constructor
+	constructor(cube) {
+		this.cube = cube;
+		this.#start();
+	}
+	// start listeners
+	#start() {
+		// set a default transform
+		this.cube.style.transform = "rotateX(0deg) rotateY(0deg)";
+		// * isMouseEnter controllers
+		// set isMouseEnter TRUE when entering the cube
+		this.cube.addEventListener("mouseenter", () => {
+			this.isMouseEnter = true;
+		});
+		// set isMouseEnter FALSE when leaving the cube
+		this.cube.addEventListener("mouseleave", () => {
+			this.isMouseEnter = false;
+		});
+		// * isMouseDown controllers
+		// set isMouseDown TRUE when : mouse is down while inside the cube
+		// and update previousTransform
+		this.cube.addEventListener("mousedown", (e) => {
+			this.isMouseDown = true;
+			this.positionInStart = { x: e.clientX, y: e.clientY };
+		});
+		// set isMouseDown et isMouseMove FALSE when mouse is up
+		window.addEventListener("mouseup", () => {
+			// if already false do nothing
+			if (this.isMouseDown == false) return;
+			this.isMouseDown = false;
+			this.isMouseMove = false;
+			this.previousTransform = this.#getCurrentTransform()
+		});
+		// * isMouseMove controllers
+		// if already isMouseMove is TRUE keep update transforming
+		// if not and all the conditions (isMouseEnter, isMouseDown) is true then set isMouseMove TRUE and update transforming
+		window.addEventListener("mousemove", (e) => {
+			if (!this.isMouseMove) {
+				if (this.isMouseEnter && this.isMouseDown) {
+					this.isMouseMove = true;
+				}
+				else { return }
+			}
+			let Δx = e.clientX - this.positionInStart.x;
+			let Δy = e.clientY - this.positionInStart.y;
+			this.#updateRotation(Δx, Δy);
+		});
+	}
+	// update transforming function
+	#updateRotation(Δx, Δy) {
+		let { x, y } = this.previousTransform
+		this.cube.style.transform = `
+		rotateX(${y - Δy / this.transforming_SCALE}deg)
+		rotateY(${x + Δx / this.transforming_SCALE}deg)`;
+	}
+	#getCurrentTransform() {
+		let [x, y] = this.cube.style.transform
+			.split(" ")
+			.map((i) => Number(i.slice(i.indexOf("(") + 1, i.indexOf("deg"))));
+		return { x, y }
+	}
 }
-let cube = new Cube(document.getElementById('cube'))
-
+let cube = new Cube(document.getElementById("cube"));
 
 // ! fix auto rotate when mouse down
